0bd4584a27ac45504d2074e1903233b8e96909e6
Cristian Klein <cristiklein@gmail.com>
2016-01-05 16:38:43 +0100

    Added git configuration

--- modules/git/manifests/init.pp
+++ modules/git/manifests/init.pp
@@ -3,4 +3,15 @@ class git {
   package { ['git', 'git-gui', 'gitk']: 
     ensure => 'installed'
   }
+
+  File {
+    owner => $real_id,
+    group => $real_id,
+  }
+ 
+  file { "${home}/.gitconfig":
+    ensure => 'present',
+    source => "puppet:///modules/${module_name}/gitconfig",
+  }
+
 }
