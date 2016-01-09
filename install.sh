0d36d896a7a8044ca58e48cfcf30a6d32e2521d1
Cristian Klein <cristiklein@gmail.com>
2016-01-09 22:39:07 +0100

    Move git-crypt unlock to provision

--- install.sh
+++ install.sh
@@ -3,16 +3,6 @@
 # bail out on any error
 set -e
 
-# test for git-crypt
-if dpkg -s git-crypt &> /dev/null; then
-   echo "git-crypt already installed"
-else
-   echo "Installing git-crypt ..."
-   sudo apt-get -y install git-crypt
-fi
-echo "Decrypting repository ..."
-git-crypt unlock
-
 # test for puppet
 if dpkg -s puppet &> /dev/null; then
    echo "Puppet already installed"
