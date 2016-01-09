a1b44263ad05ff9b39d06e7c00aba330c4a45e10
Cristian Klein <cristiklein@gmail.com>
2016-01-10 00:12:18 +0100

    Added vim config

--- modules/vim/manifests/init.pp
+++ modules/vim/manifests/init.pp
@@ -19,4 +19,19 @@ class vim {
     content => '# used the `editor` command',
   }
 
+  vcsrepo { "${home}/.vim":
+    ensure     => 'latest',
+    provider   => 'git',
+    source     => 'https://github.com/cristiklein/vimrc.git',
+    submodules => true,
+    depth      => 1,
+    user       => $real_id,
+    require    => File["${home}/.ssh/id_rsa"],
+  }
+
+  file { "${home}/.vimrc":
+    ensure => 'link',
+    target => "${home}/.vim/.vimrc",
+  }
+
 }
