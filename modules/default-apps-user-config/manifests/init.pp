be07a5036033e5dfd493751c99dbed0b00001bc3
Cristian Klein <cristiklein@gmail.com>
2016-01-14 13:35:39 +0100

    Restore launchbar

--- modules/default-apps-user-config/manifests/init.pp
+++ modules/default-apps-user-config/manifests/init.pp
@@ -10,4 +10,23 @@ class default-apps-user-config {
     source => "puppet:///modules/${module_name}/mimeapps.list",
   }
 
+  dconf::set { '/com/canonical/unity/launcher/favorites':
+    value => "[
+      'application://org.gnome.Nautilus.desktop',
+      'application://terminator.desktop',
+      'application://google-chrome.desktop',
+      'application://thunderbird.desktop',
+      'application://skype.desktop',
+      'application://libreoffice-writer.desktop',
+      'application://libreoffice-calc.desktop',
+      'application://libreoffice-impress.desktop',
+      'application://unity-control-center.desktop',
+      'unity://running-apps',
+      'unity://expo-icon',
+      'unity://devices'
+    ]",
+    user  => $real_id,
+    group => $real_id,
+  }
+
 }
