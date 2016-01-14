f6ab8ffad80137ac69537a57d421529c38ba572f
Cristian Klein <cristiklein@gmail.com>
2016-01-14 11:59:32 +0100

    Added mc config

--- modules/mc/manifests/init.pp
+++ modules/mc/manifests/init.pp
@@ -4,4 +4,18 @@ class mc {
     ensure => 'installed'
   }
 
+  File {
+    owner => $real_id,
+    group => $real_id,
+  }
+ 
+  file { "${home}/.config/mc":
+    ensure => 'directory',
+  }
+
+  file { "${home}/.config/mc/panels.ini":
+    ensure => 'present',
+    source => "puppet:///modules/${module_name}/panels.ini",
+  }
+
 }
