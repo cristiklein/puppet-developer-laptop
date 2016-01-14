ba414a2bbecfb8dfd16b8a60754cc49661d4cad2
Cristian Klein <cristiklein@gmail.com>
2016-01-14 19:44:56 +0100

    Fixed indenting

--- modules/dconf/manifests/set.pp
+++ modules/dconf/manifests/set.pp
@@ -1,27 +1,27 @@
 define dconf::set ($value,$user,$group) {
-	include dconf
+  include dconf
 
-	debug "Dconf: set $name to $value for user $user and group $group"
+  debug "Dconf: set $name to $value for user $user and group $group"
 
-	exec { "dconf set $name outside user session":
+  exec { "dconf set $name outside user session":
     command   => shellquote('dbus-launch', '--exit-with-session', 'dconf', 'write', $name, sorted_json($value)),
-		path      => "/usr/bin",
-		logoutput => "true",
+    path      => "/usr/bin",
+    logoutput => "true",
     provider  => 'shell',
-		require   => Package['dconf-tools'],
-		user      => $user,
-		group     => $group,
+    require   => Package['dconf-tools'],
+    user      => $user,
+    group     => $group,
     onlyif    => 'test -z "$DBUS_SESSION_BUS_ADDRESS"',
   }
 
-	exec { "dconf set $name inside user session":
+  exec { "dconf set $name inside user session":
     command   => shellquote('dconf', 'write', $name, sorted_json($value)),
-		path      => "/usr/bin:/bin",
-		logoutput => "true",
+    path      => "/usr/bin:/bin",
+    logoutput => "true",
     provider  => 'shell',
-		require   => Package['dconf-tools'],
-		user      => $user,
-		group     => $group,
+    require   => Package['dconf-tools'],
+    user      => $user,
+    group     => $group,
     onlyif    => 'test -n "$DBUS_SESSION_BUS_ADDRESS"',
   }
 }
