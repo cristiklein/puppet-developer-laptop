1c6b164109f9e235923f7f05edd24b2a40a312bb
Cristian Klein <cristiklein@gmail.com>
2016-01-14 11:55:27 +0100

    Moved conky config to XDG

--- modules/conky/manifests/init.pp
+++ modules/conky/manifests/init.pp
@@ -1,14 +1,25 @@
 class conky {
+
+  # Old config file location, discouraged to migrate to XDG
+  file { "${home}/.conkyrc":
+    ensure => 'absent',
+  }
   
   package { 'conky' : 
     ensure => 'installed'
   }
-  ->
-  file { "${home}/.conkyrc":
-    ensure => 'present',
-    source => "puppet:///modules/${module_name}/conkyrc",
+
+  file { "${home}/.config/conky":
+    ensure => 'directory',
+    owner  => $real_id,
+    group  => $real_id,
   }
   ->
+  file { "${home}/.config/conky/conky.conf":
+    ensure => 'present',
+    source => "puppet:///modules/${module_name}/conky.conf",
+  }
+  
   file { "${home}/.config/autostart":
     ensure => 'directory',
     owner  => $real_id,
