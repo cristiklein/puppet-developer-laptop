a4d2ac68babbbfdbae9ff3e86f005800ce73b8ea
Cristian Klein <cristiklein@gmail.com>
2016-01-26 14:12:56 +0100

    Ported java8 module to latest version of apt

--- modules/java/manifests/java_8.pp
+++ modules/java/manifests/java_8.pp
@@ -2,14 +2,7 @@ class java::java_8 {
   
   include 'apt'
   
-  apt::source { 'webupd8team': 
-    location    => 'http://ppa.launchpad.net/webupd8team/java/ubuntu',
-    release     => 'trusty',
-    repos       => 'main',
-    include_src => true,
-    key         => 'EEA14886',
-    key_server  => 'keyserver.ubuntu.com'
-  }
+  apt::ppa { 'ppa:webupd8team/java': }
   ->
   exec { 'accept-java-license':
     command => '/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections;/bin/echo /usr/bin/debconf shared/accepted-oracle-license-v1-1 seen true | sudo /usr/bin/debconf-set-selections;',
