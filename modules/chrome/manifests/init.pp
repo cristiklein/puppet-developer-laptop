fa029fb0675e31c8c2b5b11f567b21fe74786a63
Cristian Klein <cristiklein@gmail.com>
2016-01-26 10:11:27 +0100

    Ported chrome module to latest version of apt

--- modules/chrome/manifests/init.pp
+++ modules/chrome/manifests/init.pp
@@ -2,21 +2,20 @@
 class chrome {
  
   apt::key { 'chrome':
-    key        => 'A040830F7FAC5991',
-    key_source => 'https://dl-ssl.google.com/linux/linux_signing_key.pub',
+    id     => '4CCA1EAF950CEE4AB83976DCA040830F7FAC5991',
+    source => 'https://dl-ssl.google.com/linux/linux_signing_key.pub',
   }
  
   apt::source { 'google-chrome':
     location          => 'http://dl.google.com/linux/chrome/deb/',
     release           => 'stable',
     repos             => 'main',
-    include_src       => false,
-    required_packages => 'debian-keyring debian-archive-keyring',
+    include           => { src => false },
     notify            => Package ['google-chrome-stable'],
   }
  
   package { 'google-chrome-stable':
-   ensure => latest,
+    ensure => latest,
   }
  
 }
