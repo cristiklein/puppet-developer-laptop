3aa322a72e57bf637f7eb5650a414345be81758c
Cristian Klein <cristiklein@gmail.com>
2016-02-25 23:39:21 +0100

    Accept MS TTF EULA

--- modules/publishing/manifests/init.pp
+++ modules/publishing/manifests/init.pp
@@ -1,5 +1,9 @@
 class publishing {
-  
+
+  exec { 'accept-msttf-license':
+    command    => '/bin/echo /usr/bin/debconf msttcorefonts/accepted-mscorefonts-eula select true | sudo /usr/bin/debconf-set-selections;/bin/echo /usr/bin/debconf msttcorefonts/accepted-mscorefonts-eula seen true | sudo /usr/bin/debconf-set-selections;',
+    unless => '/usr/bin/dpkg -s ttf-mscorefonts-installer'
+  } ->
   package { [
     'gimp',
     'inkscape',
