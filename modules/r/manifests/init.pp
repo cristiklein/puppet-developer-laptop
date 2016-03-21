a860849d03a0231b586a3573836044d4ea82e7ca
Cristian Klein <cristiklein@gmail.com>
2016-03-21 15:28:56 +0100

    Added R studio

--- modules/r/manifests/init.pp
+++ modules/r/manifests/init.pp
@@ -1,7 +1,21 @@
+include wget
+
 class r {
   
   package { 'r-recommended':
     ensure => 'installed'
   }
+
+  wget::fetch { "RStudio Deb":
+    source      => 'https://download1.rstudio.org/rstudio-0.99.893-amd64.deb',
+    destination => '/tmp/rstudio-latest.deb',
+  }
+  ->
+  package { "rstudio":
+    provider => dpkg,
+    ensure   => latest,
+    source   => "/tmp/rstudio-latest.deb",
+  }
+
 
 }
