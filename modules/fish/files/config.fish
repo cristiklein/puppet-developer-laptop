b045773bb73aaae9e595a9f58b0d8b9b61f375ad
Cristian Klein <cristiklein@gmail.com>
2016-06-29 13:59:38 +0200

    Fixed fish script

--- modules/fish/files/config.fish
+++ modules/fish/files/config.fish
@@ -6,7 +6,7 @@ alias dev='cd ~/development'
 set -x PATH $PATH $HOME/bin
 
 # Add path to RubyGems executables
-if which ruby >/dev/null; and which gem >/dev/null; then
+if which ruby >/dev/null; and which gem >/dev/null
    set -x PATH (ruby -rubygems -e 'puts Gem.user_dir')/bin $PATH
 end
 
