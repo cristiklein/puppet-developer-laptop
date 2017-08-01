96424e098673fe18954bc33e7c9afa0c9d07100a
Cristian Klein <cristiklein@gmail.com>
2017-08-01 11:32:21 +0200

    Revert "Added path to RubyGems"
    This reverts commit 7531eddf66584e3c036ac3c9af44cd5e3f86255c.

--- modules/fish/files/config.fish
+++ modules/fish/files/config.fish
@@ -5,11 +5,6 @@ alias dev='cd ~/development'
 
 set -x PATH $PATH $HOME/bin
 
-# Add path to RubyGems executables
-if which ruby >/dev/null; and which gem >/dev/null; then
-   set -x PATH (ruby -rubygems -e 'puts Gem.user_dir')/bin $PATH
-end
-
 function update-repo --description 'Update a single APT repository'
    for source in $argv
       sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/$source" \
