13c06002bce60d9d4b8ff040f86c6cbe70a6d527
Cristian Klein <cristiklein@gmail.com>
2017-08-03 11:03:58 +0200

    Remove scala and maven

--- modules/scala/manifests/init.pp
+++ modules/scala/manifests/init.pp
@@ -10,35 +10,20 @@ class scala {
   $scala_dir = "${home}/bin/scala-2.11.4"  
   $scala_dir_exists = "/usr/bin/test -d ${scala_dir}"
 
-  # for now we'll only install a single version of scala
-  wget::fetch { 'http://downloads.typesafe.com/scala/2.11.4/scala-2.11.4.tgz':
-    destination => "/tmp/scala-2.11.4.tgz",   
-    unless      => $scala_dir_exists
+  file { "${scala_dir}":
+    ensure => 'absent',
+    force  => 'yes',
   }
-  ->
-  exec { "/bin/tar -xzf /tmp/scala-2.11.4.tgz -C ${home}/bin/":
-    unless => $scala_dir_exists, 
-    user   => $real_id,
-    group  => $real_id
-  }
-  ->
   file { "${home}/bin/scala-current":
-    ensure => 'link',
-    target => $scala_dir
+    ensure => 'absent',
   }
-  ->
   file { "${home}/bin/scala":
-    ensure => 'link',
-    target => "${home}/bin/scala-current/bin/scala"
+    ensure => 'absent',
   }
-  ->
   file { "${home}/bin/scalac":
-    ensure => 'link',
-    target => "${home}/bin/scala-current/bin/scalac"
+    ensure => 'absent',
   }
-  ->
   file { "${home}/bin/scaladoc":
-    ensure => 'link',
-    target => "${home}/bin/scala-current/bin/scaladoc"
+    ensure => 'absent',
   }
 }
