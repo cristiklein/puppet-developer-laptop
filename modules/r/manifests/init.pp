c9ced4ad32e1b4f4cb280dc39e35d1d80c523832
Cristian Klein <cristiklein@gmail.com>
2017-08-04 20:42:57 +0200

    Fix: wget module does not support hash

--- modules/r/manifests/init.pp
+++ modules/r/manifests/init.pp
@@ -8,7 +8,6 @@ class r {
 
   wget::fetch { "RStudio Deb":
     source      => 'https://download1.rstudio.org/rstudio-xenial-1.0.153-amd64.deb',
-    source_hash => 'd584cbab01041777a15d62cbef69a976',
     destination => '/tmp/rstudio-latest.deb',
   }
   ->
